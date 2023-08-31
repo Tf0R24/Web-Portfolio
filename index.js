79d1cacf9165a9f85891d377076139a93058d7ca
Tf0R24 <tomgreenan05@gmail.com>
2023-08-31 12:34:55 +0100

    Added FullPage JS
    Added and completed basic setup of FullPage.JS

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+    new fullpage('#fullpage', {
+        licenseKey: 'gplv3-license',
+        autoScrolling:true,
+        scrollHorizontally: true,
+        sectionsColor: ['red', 'orange', 'yellow', 'green', 'blue'],
+    });
+})
